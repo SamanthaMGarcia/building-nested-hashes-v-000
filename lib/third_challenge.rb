6a98d2b3013a0eb203160abd5838338ef5bf9f4c
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-11 21:50:18 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,7 +10,9 @@ def third_challenge
         :age => "54"
         },
       :hero => {
-        :
+        :name => "Romeo"
+        :age => "15"
+        
         },
       :hero_friends => []
    }, 
