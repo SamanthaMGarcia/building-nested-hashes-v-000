82b71b5241069536a922ed51f157665ceab81839
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-11 21:56:24 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,7 +14,8 @@ def third_challenge
         :age => "15",
         :status => "alive"
         },
-      :hero_friends => []
+      :hero_friends => [{name: "Benvolio", age: "17", attitude: "worried"},
+      {name: "Mercutio", age: "18", attitude: "hot-headed"}]
    }, 
    :capulet => {
       :patriarch => {
@@ -22,17 +23,15 @@ def third_challenge
         :age => "50"
         },
       :matriarch => {
-        :name => "Lady Capulet"
+        :name => "Lady Capulet",
         :age => "51"
         },
       :heroine => {
-        :name => "Juliet"
-        :age => "15"
+        :name => "Juliet",
+        :age => "15",
         :status => "alive"
         },
       :heroine_friends => []
    }
   }
-
-  
 end
